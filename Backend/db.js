86d5654f4b54fbec2bc2e571947798065c953523
Kyleth22 <vegito1200@hotmail.com>
2026-02-02 01:18:22 -0600

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-// grupo de conexion
-const pool = mysql.createPool({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-// exportacion de la conexion
-module.exports = pool.promise();
+module.exports = db;
